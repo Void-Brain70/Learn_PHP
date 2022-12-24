be340e0a76e3fb4df5c4e1dab91dfdd958a62458
Anik Chandra <67729889+Void-Brain70@users.noreply.github.com>
2022-12-24 15:46:36 +0600

    anik

---
+++ data_types.php
@@ -0,0 +1,27 @@
+<?php
+echo "Data types <br>";
+//string
+$name = "Anik Das";
+//integer
+$id = 70;
+//float
+$cg = 3.74;
+//boolean
+$t = true;
+$f = false;
+//array
+$arr = array('CSE', 'EEE');
+//null
+$n = NULL;
+
+
+echo "$name, $id, $cg <br>";
+echo " $t, $f <br>";
+echo var_dump($f);
+echo '<br>';
+echo var_dump($arr);
+echo '<br>';
+echo "$n";
+echo var_dump($n);
+
+?>
---
+++ variable.php
@@ -0,0 +1,4 @@
+<?php
+$name = "Anik Das";
+echo "My name is $name";
+?>
