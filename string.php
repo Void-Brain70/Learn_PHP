5ba396d2e84c4530be76dcc1c0a681b6a2e736b8
Anik Chandra <67729889+Void-Brain70@users.noreply.github.com>
2022-12-25 11:32:53 +0600

    anik

--- string.php
+++ string.php
@@ -1,13 +1,24 @@
 <?php
-$str = "Anik das";
+$str = "Anik Das";
+//strlen() function
 echo "String length->", strlen($str);
 echo "<br>";
+//string join with (.) operator
+echo "Anik Das"." CSE". " 18101070";
+echo "<br>";
+//str_word_count() function
 echo "Total Word->", str_word_count($str);
 echo "<br>";
+//strrev() function
 echo "Reverse String->", strrev($str);
 echo "<br>";
+//strpos() function
 echo "Serach item->", strpos($str, "das");
 echo "<br>";
+//str_replace() function
 echo "Replace item->", str_replace("das", "Koyel", "Anik das");
+echo "<br>";
+//str_repeat() function
+echo "Repeat->", str_repeat($str, 5);
 echo "<br>";
 ?>
